533e312a272f2ce5f5c1b2a2d772c54ec8c773f1
rcmorrill <rcmorrill@gmail.com>
2016-02-18 16:52:36 -0500

    week 6

--- task-1/script.js
+++ task-1/script.js
@@ -5,35 +5,46 @@ var m = {t:50,r:50,b:50,l:50},
 var data = d3.range(10).map(function(d){return Math.random()*100});
 console.log(data);
 
-var plot = d3.select('.plot').append('svg')
+
+
+var dis = d3.dispatch('pickcircle','unpickcircle');
+
+d3.selectAll('.plot')
+    .append('svg')
     .attr('width',w+ m.l+ m.r)
     .attr('height',h+ m.t + m.b)
     .append('g')
-    .attr('transform','translate('+ m.l+','+ m.t+')');
+    .attr('transform','translate('+ m.l+','+ m.t+')')
+    .each(function(d,i){
 
-var circles = plot.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('cx',function(d,i){return w/9*i})
-    .attr('cy',h/2)
-    .attr('r',10);
+        var circles = d3.select(this)
+            .selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('cx',function(d,i){return w/9*i})
+            .attr('cy',h/2)
+            .attr('r',10)
+            .on('mouseenter', function(datum,index){
+                dis.pickcircle(index);
+            })
+            .on('mouseleave', function(datum,index){
+                dis.unpickcircle(index);
+            })
+
+        dis.on('pickcircle.'+i,function(i){
+            circles.filter(function(d,_i){
+                return _i == i;
+            }).style('fill','red');
+
+        })
+
+        dis.on('unpickcircle.'+i,function(){
+            circles.style('fill',null);
+        })
+    })
 
 //Append the same 10 circles to #plot-2
-var plot2 = d3.select('#plot-2').append('svg')
-    .attr('width',w+ m.l+ m.r)
-    .attr('height',h+ m.t + m.b)
-    .append('g')
-    .attr('transform','translate('+ m.l+','+ m.t+')');
-
-var circles2 = plot2.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('cx',function(d,i){return w/9*i})
-    .attr('cy',h/2)
-    .attr('r',10);
-
 
 
 //Task: how do these two plots talk to each other?
--- task-2/script.js
+++ task-2/script.js
@@ -6,16 +6,34 @@ var queue = d3_queue.queue()
     .defer(d3.csv,'../data/hubway_stations.csv',parseStations)
     .await(dataLoaded);
 
+
+var globalDispatcher = d3.dispatch('changeinterval','changestation');
+
+d3.selectAll('.interval').on('click',function(){
+    var id = d3.select(this).attr('id');
+    globalDispatcher.changeinterval(id);
+
+})
+
+d3.select('.station-list').on('change',function(){
+    globalDispatcher.changestation(this.value)
+})
+
 // Step 1: populate UI
 // Step 2: create event dispatcher
 // Step 3: emit events and event listeners
 
 function dataLoaded(err,rows,stations){
 
+
     //create nested hierarchy based on stations
     var tripsByStation = d3.nest()
         .key(function(d){return d.startStation})
-        .entries(rows);
+        //.entries(rows);
+        .map(rows, d3.map);
+
+
+    console.log(tripsByStation.get(5))
 
     var timeSeriesModule = d3.timeSeries()
         .width(w).height(h)
@@ -25,8 +43,29 @@ function dataLoaded(err,rows,stations){
         .binSize(d3.time.week)
 
     var plots = d3.select('.container').select('.plot')
-        .datum(tripsByStation[0].values)
+        //.datum(tripsByStation[0].values)
+        .datum(tripsByStation.get(3))
         .call(timeSeriesModule);
+
+        globalDispatcher.on('changeinterval',function(interval){
+            var newInterval = interval=="d"?d3.time.day:d3.time.week;
+            timeSeriesModule.binSize(newInterval);
+            plots.call(timeSeriesModule);
+        });
+
+        globalDispatcher.on('changestation', function(id){
+            // plots.datum(tripsByStation[station].values)
+            // plots.call(timeSeriesModule);
+            //this doesn't work because the station id is not the same as the index number
+
+            plots.datum(tripsByStation.get(id))
+            plots.call(timeSeriesModule);
+
+
+             //another way to do it
+             //tripsByStation.filter(function(d){return d.key==id})
+
+        });
 
 }
 
@@ -50,5 +89,9 @@ function parseDate(date){
 }
 
 function parseStations(s){
+    d3.select('.station-list')
+        .append('option')
+        .html(s.station)
+        .attr('value', s.id);
 }
 
